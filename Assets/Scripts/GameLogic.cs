902d87ecdb2909f264efdf16c87a9bb5045466bb
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-08-22 09:17:51 -0300

    Creating the logic of the game

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public InputField userInput;
+
+    //Declaring a variable
+    //Private is an access modifier - makes this variable only
+    //accessible from this script
+    private int randomNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomNum = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Creating a function to handle the click button event
+    public void OnButtonClick() {
+
+        string userInputValue = userInput.text;
+        int answer = int.Parse(userInputValue);
+
+        answer *= 2;
+
+        //Debug.Log("Button clicked");
+        Debug.Log("Value of Input: " + userInput.text);
+        Debug.Log("The valor of randoNum: " + randomNum);
+        Debug.Log("Value of answer: " + answer);
+
+    }
+
+}
